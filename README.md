039359a40da85a1718fbbbde2b8921ad2c6ecd90
skasugantivenkata <skasugantivenkata@deloitte.com>
2020-08-20 12:38:48 +0530

    MongoDB CRUD operations example
    MongoDB CRUD operations example

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # task-manager
+
+/Users/skasugantivenkata/mongodb/bin/mongod --dbpath=/Users/skasugantivenkata/mongodb-data
+db.collection('name');
+db.collection('name').insertOne(record,callback)
+                     .insertMany()
+                     .findOne() ->FindOne always returns the first match
+                     .find() -> returns cursor/ pointer to the records
+
+                     
+object ID (_id) in Mongo db records:
+
+4 byte -> time stamp
+5 bytes -> random vaalue
+3 byte -> counter starting with random value
+
+ID is stored in binary format as string takes almost double storage to store
+
--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,18 @@
 // CRUD create read update delete
+const {MongoClient, ObjectID} = require('mongodb')
+// --- or -----
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectID;
 
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
 
 const connectionURL = 'mongodb://127.0.0.1:27017'; // local host IP and port, 
 const databaseName = 'task-manager';
+
+// object Id generation
+const id = new ObjectID();
+// console.log(id,'----',id.getTimestamp());
+
 //connecting to DB
 MongoClient.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology:true}, (error, client) => {
     // remove  useUnifiedTopology:true if it behaves wierdly 
@@ -14,8 +22,83 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology:tr
     console.log('Connected to DB');
     const db = client.db(databaseName);
 
+    // DELETE
+    // const deleteOnePromise = db.collection('users').deleteOne({age: 23});
+    // deleteOnePromise.then((result)=> {
+    //     console.log(result);
+    //     printDBvalues('users');
+    // }).catch((error)=>{
+    //     console.log(error,'----Error in deleting the record');
+    // })
+
+    //Delete Many
+    const deleteManyPromise = db.collection('users').deleteMany({age: 29});
+    deleteManyPromise.then((result)=> {
+        console.log(result.deletedCount);
+        printDBvalues('users');
+    }).catch((error)=>{
+        console.log(error,'----Error in deleting the record');
+    })
+
+    // UPDATE
+
+    // const updatePromise = db.collection('tasks').updateOne({
+    //     _id: new ObjectID('5f3d59f87d09abe31c7057b2')
+    //     // description: ''
+    // }, {
+    //     $set: {
+    //         description: 'Diary'
+    //     }
+    // })
+
+    // updatePromise.then((result)=> {
+    //     console.log(result.matchedCount);
+    //     printDBvalues('tasks');
+    // }).catch((error)=> {
+    //     console.log(error,'----Error in updatinng the doc');
+    // })
+
+    // const updateManyPromise = db.collection('users').updateMany({age: '28'}, 
+    //     {
+    //         $set: {
+    //             age: 28
+    //         }
+    //     }
+    //     );
+
+    // updateManyPromise.then((result)=> {
+    //     console.log(result.modifiedCount)
+    //     printDBvalues('users');
+    // }).catch((error)=> {
+    //     console.log(error,'----Error in updatinng the doc');
+    // })
+
+
+    // FIND / READ
+    // db.collection('tasks').findOne({ _id: new ObjectID('5f3cd2c0d627b174414ea4c7')}, (error, user) => {
+    //     if(error) {
+    //         return console.log('Unable to fetch');
+    //     }
+    //     console.log(user,'-----');
+    // });
+    const printDBvalues = (dbName) => {    db.collection(dbName).find({ }).toArray((error, users) => {
+        if(error) {
+            return console.log('Unable to fetch');
+        }
+        console.log(users);
+    }) }
+ 
+    // db.collection('tasks').find({ completed: true }).count((error, count) => {
+    //     if(error) {
+    //         return console.log('Unable to fetch');
+    //     }
+    //     console.log(count);
+    // });
+
+    // CREATE / INSERT
     // insert one record
     // db.collection('users').insertOne({
+    //     _id: id,
     //     name: 'Sneha',
     //     age: 23
     // }, (error, result) => {
@@ -25,37 +108,19 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology:tr
     //     console.log(result.ops);
     // })
 
-    // insert multiple records
 
-    // db.collection('users').insertMany(
-    //     [
-    //         {
-    //             name: 'Rama',
-    //             age: 24
-    //         }, {
-    //             name: 'Lahari',
-    //             age: 16
-    //         }
-    //     ],
+    // creating new tasks application- multiple records
+    // var tasksList = [{ description: 'Exercise', completed: true}, { description: 'Complete Node Course', completed: false},  { description: 'Green Tea', completed: true} ]
+    // db.collection('tasks').insertMany(
+    //     tasksList,
     //     (error, result) => {
     //         if(error) {
-    //             return console.log('oops! unable to connect');
+    //             return console.log('Oops! unable to create records');
+    //         } else {
+    //             console.log(result.ops);
     //         }
-
-    //         console.log(result.ops);
     //     }
     // )
 
-    // creating new tasks application
-    var tasksList = [{ description: 'Exercise', completed: true}, { description: 'Complete Node Course', completed: false},  { description: 'Green Tea', completed: true} ]
-    db.collection('tasks').insertMany(
-        tasksList,
-        (error, result) => {
-            if(error) {
-                return console.log('Oops! unable to create records');
-            } else {
-                console.log(result.ops);
-            }
-        }
-    )
+  
 })
